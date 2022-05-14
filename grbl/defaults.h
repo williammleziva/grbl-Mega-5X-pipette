0618607c02bf7ebb67731c81042b3188153d88f5
Gauthier <fra589@users.noreply.github.com>
2022-05-14 18:40:14 +0200

    reduce homing feed and seek for more accuracy

--- grbl/defaults.h
+++ grbl/defaults.h
@@ -165,8 +165,8 @@
     #define DEFAULT_LASER_MODE 0 // false
     #define DEFAULT_HOMING_ENABLE 1 // true
     #define DEFAULT_HOMING_DIR_MASK 0 // move positive dir
-    #define DEFAULT_HOMING_FEED_RATE 100.0 // mm/min
-    #define DEFAULT_HOMING_SEEK_RATE 500.0 // mm/min
+    #define DEFAULT_HOMING_FEED_RATE 25.0 // mm/min
+    #define DEFAULT_HOMING_SEEK_RATE 250.0 // mm/min
     #define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
     #define DEFAULT_HOMING_PULLOFF 5.0 // mm
   #endif 
